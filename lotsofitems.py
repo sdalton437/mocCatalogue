fd5c94bac0b9abae8ed3566cdf292d5ca15be615
sdalton437 <daltonsam437@gmail.com>
2017-09-26 21:04:32 -0400

    Update lotsofitems.py

--- lotsofitems.py
+++ lotsofitems.py
@@ -12,87 +12,100 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-sport1 = Sport(name = 'Whitewater Kayaking')
+sport1 = Sport(name='Whitewater Kayaking',
+               picture='/static/img/whitewaterkayaking.jpg')
 
 session.add(sport1)
 session.commit()
 
-item1 = Item(sport = sport1, name = 'Whitewater Paddle', description = 'A paddle used to propel and maneuver whitewater kayaks. Primary use is for whitewater')
+item1 = Item(sport=sport1, name='Whitewater Paddle',
+             description='A paddle used to propel and maneuver whitewater kayaks. Primary use is for whitewater',
+             picture='/static/img/whitewaterpaddle.jpg')
 
 session.add(item1)
 session.commit()
 
-item2 = Item(sport = sport1, name = 'Whitewater Sprayskirt', description = 'A waterproof cover for a whitewater kayak, that prevents water from entering the boat while still allowing the paddler to maneuver')
+item2 = Item(sport=sport1, name='Whitewater Sprayskirt',
+             description='A waterproof cover for a whitewater kayak, that prevents water from entering the boat while still allowing the paddler to maneuver', picture='/static/img/whitewaterkayakskirt.jpg')
 
 session.add(item2)
 session.commit()
 
-item3 = Item(sport = sport1, name = 'Whitewater Kayak', description = 'A kayak meant to be used in whitewater')
+item3 = Item(sport=sport1, name='Whitewater Kayak',
+             description='A kayak meant to be used in whitewater', picture='/static/img/whitewaterkayak.jpg')
 
 session.add(item3)
 session.commit()
 
-sport2 = Sport(name = 'Rock Climbing')
+sport2 = Sport(name='Rock Climbing', picture='/static/img/rockclimbing.jpg')
 
 session.add(sport2)
 session.commit()
 
-item4 = Item(sport = sport2, name = 'Climbing Rope', description = 'Rope used for climbing')
+item4 = Item(sport=sport2, name='Climbing Rope',
+             description='Rope used for climbing', picture='/static/img/climbingrope.jpg')
 
 session.add(item4)
 session.commit()
 
-item5 = Item(sport = sport2, name = 'Climbing Harness', description = 'Used to secure climber to rope')
+item5 = Item(sport=sport2, name='Climbing Harness',
+             description='Used to secure climber to rope', picture='/static/img/climbingharness.jpg')
 
 session.add(item5)
 session.commit()
 
-item6 = Item(sport = sport2, name = 'Climbing Shoes', description = 'Shoes used for climbing')
+item6 = Item(sport=sport2, name='Climbing Shoes',
+             description='Shoes used for climbing', picture='/static/img/climbingshoes.jpg')
 
 session.add(item6)
 session.commit()
 
-sport3 = Sport(name = 'Ice Climbing')
+sport3 = Sport(name='Ice Climbing', picture='/static/img/iceclimbing.jpg')
 
 session.add(sport3)
 session.commit()
 
-item7 = Item(sport = sport3, name = 'Ice Tool', description = 'Tool used to ascend ice')
+item7 = Item(sport=sport3, name='Ice Tool', description='Tool used to ascend ice',
+             picture='/static/img/iceclimbingicetool.jpg')
 
 session.add(item7)
 session.commit()
 
-item8 = Item(sport = sport3, name = 'Ice Crampons', description = 'Crampons used for ice climbing')
+item8 = Item(sport=sport3, name='Ice Crampons', description='Crampons used for ice climbing',
+             picture='/static/img/iceclimbingcrampons.jpg')
 
 session.add(item8)
 session.commit()
 
-item9 = Item(sport = sport3, name = 'Mountainering Boots', description = 'Boots used for mountaineering sports')
+item9 = Item(sport=sport3, name='Mountainering Boots', description='Boots used for mountaineering sports',
+             picture='/static/img/iceclimbingmountaineeringboots.jpg')
 
 session.add(item9)
 session.commit()
 
-sport4 = Sport(name = 'Camping')
+sport4 = Sport(name='Camping', picture='/static/img/camping.jpg')
 
 session.add(sport4)
 session.commit()
 
-item10 = Item(sport = sport4, name = 'Tent', description = 'Makeshift shelter used for the outdoors')
+item10 = Item(sport=sport4, name='Tent', description='Makeshift shelter used for the outdoors',
+              picture='/static/img/campingtent.jpg')
 
 session.add(item10)
 session.commit()
 
-item11 = Item(sport = sport4, name = 'Sleeping Bag', description = 'Bag used for sleeping in the wilderness')
+item11 = Item(sport=sport4, name='Sleeping Bag', description='Bag used for sleeping in the wilderness',
+              picture='/static/img/campingsleepingbag.jpg')
 
 session.add(item11)
 session.commit()
 
-item12 = Item(sport = sport4, name = 'Sleeping Mat', description = 'Mat used to seperate user from the bare ground while camping')
+item12 = Item(sport=sport4, name='Sleeping Mat', description='Mat used to seperate user from the bare ground while camping',
+              picture='/static/img/campingsleepingmat.jpg')
 
 session.add(item12)
 session.commit()
 
 
-print "added menu items!"
 
-
+print "added catalogue items!"
