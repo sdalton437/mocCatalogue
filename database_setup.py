90e322ba1c1c209d0b155ccc3c1a6c05a81745fe
sdalton437 <daltonsam437@gmail.com>
2017-09-26 21:05:19 -0400

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -7,20 +7,50 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(45), nullable=False)
+    picture = Column(String(512))
+
+
 class Sport(Base):
-	__tablename__ = 'sport'
-	name = Column(String(250), nullable=False)
-	id = Column(Integer, primary_key=True)
+    __tablename__ = 'sport'
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    picture = Column(String(512))
+
+    @property
+    def serialize(self):
+        # Returns object data in easily serializable format
+        return {
+            'name': self.name,
+            'id': self.id,
+            'picture': self.picture,
+        }
 
 
 class Item(Base):
-	__tablename__ = 'item'
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
-	sport_id = Column(Integer, ForeignKey('sport.id'))
-	sport = relationship(Sport)
+    __tablename__ = 'item'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    sport_id = Column(Integer, ForeignKey('sport.id'))
+    sport = relationship(Sport)
+    picture = Column(String(512))
 
+    @property
+    def serialize(self):
+        # Returns object data in easily serializable format
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'picture': self.picture,
+        }
 engine = create_engine('sqlite:///catalogitems.db')
 
 Base.metadata.create_all(engine)
