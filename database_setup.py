738242a32b99bbe4eb823cbbbcc184e16453961a
sdalton437 <daltonsam437@gmail.com>
2017-09-30 14:45:38 -0400

    Added cascade

--- database_setup.py
+++ database_setup.py
@@ -22,6 +22,7 @@ class Sport(Base):
     name = Column(String(250), nullable=False)
     id = Column(Integer, primary_key=True)
     picture = Column(String(512))
+    item = relationship(Item, cascade="delete")
 
     @property
     def serialize(self):
@@ -43,6 +44,7 @@ class Item(Base):
     picture = Column(String(512))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
     @property
     def serialize(self):
         # Returns object data in easily serializable format
